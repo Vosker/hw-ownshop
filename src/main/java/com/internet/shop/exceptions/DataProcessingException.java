5bf6eccbbcaa10326c2b9926da61ad326f8dc90a
vosker <nikita.vaganov16@gmail.com>
2020-09-21 16:08:53 +0300

    Added: -connetion to db -ProductDaoJdbcImpl to for SQL -war to jar in pom.xml -changed tests to pass only for ProductDao

---
+++ src/main/java/com/internet/shop/exceptions/DataProcessingException.java
@@ -0,0 +1,7 @@
+package com.internet.shop.exceptions;
+
+public class DataProcessingException extends RuntimeException{
+    public DataProcessingException(String message, Throwable exception) {
+        super(message, exception);
+    }
+}
---
+++ src/main/java/com/internet/shop/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package com.internet.shop.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find mySQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        Properties dbProperties = new Properties();
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "1234");
+        String url = "jdbc:mysql://localhost:3306/own_shop?serverTimezone=UTC";
+        try {
+            return DriverManager.getConnection(url, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't established the connection to DB", e);
+        }
+    }
+}
