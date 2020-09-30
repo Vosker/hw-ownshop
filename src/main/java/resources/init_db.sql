85f2d8b0798ca0ba651436325a0a83c13eb62888
vosker <nikita.vaganov16@gmail.com>
2020-09-30 07:43:11 +0300

    small corrections

--- src/main/java/resources/init_db.sql
+++ src/main/java/resources/init_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE `own-shop`.`products`
     `product_id`      BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `name`    VARCHAR(255) NOT NULL,
     `price`   DOUBLE       NOT NULL,
-    `isDeleted` TINYINT    NOT NULL NULL DEFAULT 0,
+    `isDeleted` TINYINT    NOT NULL DEFAULT 0,
     PRIMARY KEY (`product_id`)
 );
 
