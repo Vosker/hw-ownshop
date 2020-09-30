6d9ba0b4fac89b27df6e2f3a5ca4f4424548d1a2
vosker <nikita.vaganov16@gmail.com>
2020-09-30 07:44:49 +0300

    small corrections

--- src/main/java/resources/init_db.sql
+++ src/main/java/resources/init_db.sql
@@ -2,10 +2,10 @@ CREATE SCHEMA `own-shop` DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE `own-shop`.`products`
 (
-    `product_id`      BIGINT(11)   NOT NULL AUTO_INCREMENT,
-    `name`    VARCHAR(255) NOT NULL,
-    `price`   DOUBLE       NOT NULL,
-    `isDeleted` TINYINT    NOT NULL DEFAULT 0,
+    `product_id`  BIGINT(11)   NOT NULL AUTO_INCREMENT,
+    `name`        VARCHAR(255) NOT NULL,
+    `price`       DOUBLE       NOT NULL,
+    `isDeleted`   TINYINT    NOT NULL DEFAULT 0,
     PRIMARY KEY (`product_id`)
 );
 
